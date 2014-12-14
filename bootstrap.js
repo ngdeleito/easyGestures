2958176e662c21c169866549ec5f0b965d1cad66
Nicolás González-Deleito <ngdeleito@github>
2014-12-14 17:41:32 +0100

    created version 4.8

--- bootstrap.js
+++ bootstrap.js
@@ -144,6 +144,9 @@ function startup(data, reason) {
       if (Services.vc.compare(data.oldVersion, "4.7") < 0) {
         eGPrefs.updateToV4_7();
       }
+      if (Services.vc.compare(data.oldVersion, "4.8") < 0) {
+        eGPrefs.updateToV4_8();
+      }
     }
     
     // getting access to localization strings
