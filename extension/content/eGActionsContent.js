f347973c14a925c9726d24681fdb97b3ab4ceac0
Nicolás González-Deleito <ngdeleito@github>
2020-12-11 19:32:29 +0100

    upgraded license for non-graphical assets to MPL 2.0

--- extension/content/eGActionsContent.js
+++ extension/content/eGActionsContent.js
@@ -1,38 +1,11 @@
-/***** BEGIN LICENSE BLOCK *****
-Version: MPL 1.1/GPL 2.0/LGPL 2.1
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-The contents of this file are subject to the Mozilla Public License Version 
-1.1 (the "License"); you may not use this file except in compliance with 
-the License. You may obtain a copy of the License at 
-http://www.mozilla.org/MPL/
-
-Software distributed under the License is distributed on an "AS IS" basis,
-WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-for the specific language governing rights and limitations under the
-License.
-
-The Original Code is easyGestures N.
-
-The Initial Developer of the Original Code is ngdeleito.
-
-Contributor(s):
-  Jens Tinz, his portions are Copyright (C) 2002. All Rights Reserved.
-  Ons Besbes.
-
-Alternatively, the contents of this file may be used under the terms of
-either the GNU General Public License Version 2 or later (the "GPL"), or
-the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-in which case the provisions of the GPL or the LGPL are applicable instead
-of those above. If you wish to allow use of your version of this file only
-under the terms of either the GPL or the LGPL, and not to allow others to
-use your version of this file under the terms of the MPL, indicate your
-decision by deleting the provisions above and replace them with the notice
-and other provisions required by the GPL or the LGPL. If you do not delete
-the provisions above, a recipient may use your version of this file under
-the terms of any one of the MPL, the GPL or the LGPL.
-
-***** END LICENSE BLOCK *****/
-
+/* The current maintainer of this file (ngdeleito) suspects that the original
+   version of some code excerpts might be by Jens Tinz, Copyright (C) 2002. All
+   Rights Reserved. The excerpts are explicitly marked where they occur in this
+   file. */
 
 /* exported actionStatusSetters, actionRunners */
 /* global document, ACTIONS_NODE_ID_PREFIX, TOOLTIPS_NODE_ID_PREFIX,
@@ -119,6 +92,8 @@ let actionRunners = {
   },
   
   zoomIn: function() {
+    // the original version of the code of this function might be by Jens Tinz,
+    // Copyright (C) 2002. All Rights Reserved.
     // double image size only
     let width = imageElement.style.width === "" ?
       imageElement.width * 2 + "px" :
@@ -133,6 +108,8 @@ let actionRunners = {
   },
   
   zoomOut: function() {
+    // the original version of the code of this function might be by Jens Tinz,
+    // Copyright (C) 2002. All Rights Reserved.
     // halve image size only
     let width = imageElement.style.width === "" ?
       imageElement.width * 0.5 + "px" :
--- extension/content/eGPieMenu.js
+++ extension/content/eGPieMenu.js
@@ -1,40 +1,11 @@
-/***** BEGIN LICENSE BLOCK *****
-Version: MPL 1.1/GPL 2.0/LGPL 2.1
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-The contents of this file are subject to the Mozilla Public License Version 
-1.1 (the "License"); you may not use this file except in compliance with 
-the License. You may obtain a copy of the License at 
-http://www.mozilla.org/MPL/
-
-Software distributed under the License is distributed on an "AS IS" basis,
-WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-for the specific language governing rights and limitations under the
-License.
-
-The Original Code is RadialContext.
-
-The Initial Developer of the Original Code is Jens Tinz.
-Portions created by the Initial Developer are Copyright (C) 2002
-the Initial Developer. All Rights Reserved.
-
-Contributor(s):
-  Ons Besbes.
-  ngdeleito
-  
-Alternatively, the contents of this file may be used under the terms of
-either the GNU General Public License Version 2 or later (the "GPL"), or
-the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-in which case the provisions of the GPL or the LGPL are applicable instead
-of those above. If you wish to allow use of your version of this file only
-under the terms of either the GPL or the LGPL, and not to allow others to
-use your version of this file under the terms of the MPL, indicate your
-decision by deleting the provisions above and replace them with the notice
-and other provisions required by the GPL or the LGPL. If you do not delete
-the provisions above, a recipient may use your version of this file under
-the terms of any one of the MPL, the GPL or the LGPL.
-
-***** END LICENSE BLOCK *****/
-
+/* The current maintainer of this file (ngdeleito) suspects that the original
+   version of some code excerpts might be by Jens Tinz, Copyright (C) 2002. All
+   Rights Reserved. The excerpts are explicitly marked where they occur in this
+   file. */
 
 /* global browser, contextualMenus, document, addEventListener,
           removeMenuEventHandler, actionStatusSetters, window, handleMousemove,
@@ -585,6 +556,8 @@ let eGPieMenu = {
     
     this._hideLinkSign();
     
+    // the original version of the code of the remainder of this function might
+    // be by Jens Tinz, Copyright (C) 2002. All Rights Reserved.
     // state change if was dragged
     if (this.isJustOpened() && (movementX !== 0 || movementY !== 0)) {
       this._setJustOpenedAndMouseMoved();
