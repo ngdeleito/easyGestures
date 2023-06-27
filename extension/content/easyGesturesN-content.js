66aeac171669d609b2846274c98659faaea4290f
Nicolás González-Deleito <ngdeleito@github>
2023-06-27 19:42:45 +0200

    fixed unused variable warning raised by eslint

--- extension/content/easyGesturesN-content.js
+++ extension/content/easyGesturesN-content.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-/* exported removeMenuEventHandler, handleMousemove */
+/* exported imageElement, inputElement, removeMenuEventHandler, handleMousemove */
 /* global window, addEventListener, browser, eGPieMenu, document, MouseEvent,
           KeyboardEvent, actionRunners, removeEventListener */
 
